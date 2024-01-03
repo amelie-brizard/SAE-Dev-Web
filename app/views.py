62c461e63096b00892d36cc8c692a4e6a40c09c5
amelie_brizard <amelie.brizard@etu.univ-orleans.fr>
2024-01-03 01:13:39 +0100

    Page moncompte

--- app/views.py
+++ app/views.py
@@ -51,48 +51,48 @@ from hashlib import sha256
 
 @app.route("/")
 def accueil():
-    return render_template("accueil.html", accueil=True, id_page=0)
+    return render_template("accueil.html", accueil=True, id_page=0, moncompte=False)
 
 @app.route("/programmation/")
 def programmation():
-    return render_template("programmation.html", accueil=False, id_page=1)
+    return render_template("programmation.html", accueil=False, id_page=1, moncompte=False)
 
 @app.route("/reservation/")
 @login_required
 def reservation():
-    return render_template("reservation.html", accueil=False, id_page=2)
+    return render_template("reservation.html", accueil=False, id_page=2, moncompte=False)
 
 @app.route("/connexion/")
 def connexion():
-    return render_template("connexion.html", accueil=False, id_page=0)
+    return render_template("connexion.html", accueil=False, id_page=0, moncompte=False)
 
 @app.route("/inscription/")
 def inscription():
-    return render_template("inscription.html", accueil=False, id_page=0)
+    return render_template("inscription.html", accueil=False, id_page=0, moncompte=False)
 
 @app.route("/moncompte/")
-@login_required
+# @login_required
 def espace_compte():
-    return render_template("espace-compte.html", accueil=False, id_page=0)
+    return render_template("espace-compte.html", accueil=False, id_page=0, moncompte=True)
 
-@app.route("/moncompte/editioninformations/")
-@login_required
+@app.route("/moncompte/mesinformations/")
+# @login_required
 def edition_informations():
-    return render_template("compte-infos.html", accueil=False, id_page=0)
+    return render_template("compte-infos.html", accueil=False, id_page=0, moncompte=False)
 
 @app.route("/moncompte/mesfavoris/")
 @login_required
 def mes_favoris():
-    return render_template("favoris.html", accueil=False, id_page=0)
+    return render_template("favoris.html", accueil=False, id_page=0, moncompte=False)
 
 @app.route("/moncompte/mesbillets/")
 @login_required
 def mes_billets():
-    return render_template("billets-achetes.html", accueil=False, id_page=0)
+    return render_template("billets-achetes.html", accueil=False, id_page=0, moncompte=False)
 
 @app.route("/infosartiste/")
 def infos_artiste():
-    return render_template("infos-artistes.html", accueil=False, id_page=0)
+    return render_template("infos-artistes.html", accueil=False, id_page=0, moncompte=False)
 
 # @app.route("/login/", methods=("GET","POST",))
 # def login():
